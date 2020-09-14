d898c6ae8f38adf9406b0d066977e1d0ffb3e987
lif300 <luisferbarron@gmail.com>
2020-09-14 17:30:14 -0500

    Proyecto DM primer Commit (Prueba)

---
+++ app/src/main/java/com/example/marketplaceproject/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.marketplaceproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
